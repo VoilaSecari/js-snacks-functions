cca110a7b36c1abd9ea2b49b5a764b7a3e686515
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-04-08 16:17:09 +0200

    boilerplate + saluti

---
+++ 01_saluti/script.js
@@ -0,0 +1,11 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = "Mario";
+
+// Dichiara la funzione qui.
+const userNameHello = (userName) => "Ciao " + userName;
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(userNameHello(userName));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
