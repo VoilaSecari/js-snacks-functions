adfb6c890634a18408f367cd4950d0aec73fb83b
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-04-08 16:34:20 +0200

    esercizio iniziali completato

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function initialsList(listName) {
+  const namesInitials = [];
+  for (let i = 0; i < listName.length; i++) {
+    const currentInitial = listName[i];
+    namesInitials.push(currentInitial[0]);
+  }
 
+  return namesInitials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initialsList(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
