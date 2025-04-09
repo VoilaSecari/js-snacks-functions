a27fe43a84d1a09c71178d387ee484222c78b5e8
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-04-09 10:23:45 +0200

    vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+const countVowels = (text) => {
+  let numberOfVowels = 0;
+  for (let i = 0; i < text.length; i++) {
+    const currentChar = text[i][0].toLowerCase();
+    if (
+      currentChar === "a" ||
+      currentChar === "e" ||
+      currentChar === "i" ||
+      currentChar === "o" ||
+      currentChar === "u"
+    ) {
+      numberOfVowels++;
+    }
+  }
+  return numberOfVowels;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(countVowels(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
